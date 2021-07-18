223070801cf3f5ea32762a9bc28d7b80170f876c
hadar-g <hadar.goldstein@gmail.com>
2021-07-18 13:04:04 -0700

    added styling to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 
 import mealsImg from '../../Assets/meals.jpeg'
+import classes from './Header.module.css'
 
 const Header = props => {
     return(<>
-    <header>
+    <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button> cart </button>
     </header>
-        <div>
+        <div className={classes['main-image']}>
             <img src={mealsImg} alt =" a table full of food" />
         </div>
     </>)
