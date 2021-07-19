4c5ac9724d6a3224888efbf3d8ab263570ca333b
hadar-g <hadar.goldstein@gmail.com>
2021-07-19 15:06:43 -0700

    writing cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,13 @@
 import classes from 'Cart.module.css'
 
+const cartItems = <ul className = {classes['cart-tiems']}>{[{id: 'c1', name: 'sushi', amount : 2, price: 12.99}].map(item => <li>item.name</li>)}</ul>
 const Cart = props => {
     return(
-        <>
-        </>
+        <div>
+            {cartItems}
+            <div></div>
+            <div></div>
+        </div>
     )
 }
 
