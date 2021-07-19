a0f49e48a49cc4462eed1ce66239465eeb14b424
hadar-g <hadar.goldstein@gmail.com>
2021-07-18 20:41:41 -0700

    meal item submit form

---
+++ src/components/Meals/MealItemForm.js
@@ -0,0 +1,13 @@
+import classes from './MealItemForm.module.css'
+
+const MealItemForm = props => {
+    return(
+    <form className = {classes.form}>
+        <input />
+        <button> + add</button>
+    </form>
+    )
+
+}
+
+export default MealItemForm
