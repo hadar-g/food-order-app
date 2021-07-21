2ee1d4d24d74eacdd9866d7ddfa88d50c40e9b46
hadar-g <hadar.goldstein@gmail.com>
2021-07-21 11:05:09 -0700

    fixed bugs

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -3,33 +3,32 @@ import classes from './MealItem.module.css'
 import { useContext } from 'react'
 import CartContext from '../../store/cart-context'
 
-
-const MealItem = props => {
-
-    const cartCTX = useContext(CartContext)
-
-    const price = `$${props.price.toFixed(2)}`
-
-    const addToCartHandler = amount => {
-        cartCTX.addItem({
-            id: props.id,
-            name: props.name,
-            amound: amount,
-            price: props.price,
-        })
-    }
-
-    return <li className = {classes.meal}>
+const MealItem = (props) => {
+    const cartCtx = useContext(CartContext);
+  
+    const price = `$${props.price.toFixed(2)}`;
+  
+    const addToCartHandler = (amount) => {
+      cartCtx.addItem({
+        id: props.id,
+        name: props.name,
+        amount: amount,
+        price: props.price,
+      });
+    };
+  
+    return (
+      <li className={classes.meal}>
         <div>
-            <h3>{props.name}</h3>
-            <div className = {classes.description}>{props.description}</div>
-            <div className = {classes.price}>{price}</div>
+          <h3>{props.name}</h3>
+          <div className={classes.description}>{props.description}</div>
+          <div className={classes.price}>{price}</div>
         </div>
-            <MealItemForm  onAddToCart={addToCartHandler}/>
         <div>
-
+          <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
         </div>
-    </li>
-}
-
-export default MealItem
+      </li>
+    );
+  };
+  
+  export default MealItem;
