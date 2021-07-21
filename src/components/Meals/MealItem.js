97ebfe8c7427a565c929964ee5f4858c4f151c2a
hadar-g <hadar.goldstein@gmail.com>
2021-07-20 22:51:57 -0700

    adding cart context to meal item components to update when meal is added, bug showing NaN on cart badge

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,17 +1,31 @@
 import MealItemForm from './MealItemForm'
 import classes from './MealItem.module.css'
+import { useContext } from 'react'
+import CartContext from '../../store/cart-context'
 
 
 const MealItem = props => {
 
+    const cartCTX = useContext(CartContext)
+
     const price = `$${props.price.toFixed(2)}`
+
+    const addToCartHandler = amount => {
+        cartCTX.addItem({
+            id: props.id,
+            name: props.name,
+            amound: amount,
+            price: props.price,
+        })
+    }
+
     return <li className = {classes.meal}>
         <div>
             <h3>{props.name}</h3>
             <div className = {classes.description}>{props.description}</div>
             <div className = {classes.price}>{price}</div>
         </div>
-            <MealItemForm />
+            <MealItemForm  onAddToCart={addToCartHandler}/>
         <div>
 
         </div>
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,11 +1,33 @@
 import classes from './MealItemForm.module.css'
 import Input from '../UI/Input'
-
+import {useRef, useState} from 'react'
 
 const MealItemForm = props => {
+    const[amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef()
+
+    const submitHandler = event => {
+        event.preventDefault()
+
+        const enteredAmount = amountInputRef.current.value
+        const enteredAmountNumber = +enteredAmount
+
+        if(enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            setAmountIsValid(false)
+            return
+        }
+
+        props.onAddToCart(enteredAmountNumber)
+    }
+
+    
+
     return(
-    <form className = {classes.form}>
-        <Input label = "Amount" input = {{
+    <form className = {classes.form} onSubmit={submitHandler}>
+        <Input 
+        ref = {amountInputRef}
+        label = "Amount" 
+        input = {{
             id: 'amount',
             type: 'number',
             min: '1',
@@ -14,6 +36,7 @@ const MealItemForm = props => {
             defaultValue: '1'
         }} />
         <button> + add</button>
+        {!amountIsValid && <p>Please enter a valud amount (1-5)</p>}
     </form>
     )
 
